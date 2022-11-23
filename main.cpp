8e9dad8de517f8c8be1dae02e1c1f1eb458d5376
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-23 15:18:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   int n,p=1,s=1,t;
+  cin>>n;
+  if (n>=2) {
+      cout<<p<<endl<<s<<endl;
+      for(int c=3;c<=n;c++){
+          t=p+s;
+          p=s;
+          s=t;
+          cout<<t<<endl;
+      }
+  } else {
+      cout<<"errore";
+  }
    return 0;
 }
